8a97b1bd5f6a29b26678d2d48b0b011f891da911
schirmr <schirmr@outlook.com>
2026-03-20 15:02:24 -0300

    add esp code

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 Elaboração de uma fechadura eletrônica para acesso ao laboratório do Centro de Tecnologia.
 
+## Programação do ESP-8266
+
+Integrado no sistema do ESP a gravação Over-The-Air (OTA)
+
 ## Funcionalidades
 
 ## Componentes
---
+++ code-esp8266/code-esp8266.ino
@@ -0,0 +1,50 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
+const char* ssid = "TauraBots";
+const char* password = "robotica2024";
+
+void setup() {
+  pinMode(D4, OUTPUT);
+  Serial.begin(115200);
+  Serial.println("Iniciando...");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Conexao falhou! Reiniciando...");
+    delay(5000);
+    ESP.restart();
+  }
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("Inicio...");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("nFim!");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progresso: %u%%r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Erro [%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Autenticacao Falhou");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Falha no Inicio");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Falha na Conexao");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Falha na Recepcao");
+    else if (error == OTA_END_ERROR) Serial.println("Falha no Fim");
+  });
+  ArduinoOTA.begin();
+  Serial.println("Pronto");
+  Serial.print("Endereco IP: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  ArduinoOTA.handle();
+  digitalWrite(D4, HIGH); // Aciona sinal 2
+  delay(2000); // Espera por 2 segundos
+  digitalWrite(D4, LOW); // Apaga sinal 2
+  delay(2000);  // Espera por 2 segundos
+}
